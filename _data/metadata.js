fbd7b59ba97423548d8bc1582628e41b3906971a
betesda-brasilia <128605652+betesda-brasilia@users.noreply.github.com>
2025-08-31 09:38:51 -0300

    Update _data/metadata.js (via Pages CMS)

--- _data/metadata.js
+++ _data/metadata.js
@@ -2,9 +2,9 @@ module.exports = {
 	title: "Betesda Brasília",
 	url: "https://www.betesdabrasilia.org/",
 	language: "pt-br",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "Somos uma Comunidade Cristã que anuncia o Deus que nos resgatou por seu amor. Nosso amor a Deus se concretiza no amor ao próximo.",
 	author: {
 		name: "Betesda Brasília",
-		url: "https://www.betesdabrasilia.org/about-me/"
+		url: "https://www.betesdabrasilia.org/"
 	}
 }
